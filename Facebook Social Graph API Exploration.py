6be72b4a797c985005fb48f73c635ebd5cf64c3f
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2026-04-09 00:12:24 +0530

    Facebook Social Graph API Exploration.py

--- Facebook Social Graph API Exploration.py
+++ Facebook Social Graph API Exploration.py
@@ -2,15 +2,12 @@
 
 import requests
 
-# Paste your access token here
 access_token = "Facebook_Access_Token"
 
-# Get basic profile data
 url = "https://graph.facebook.com/me?access_token=" + access_token
 
 response = requests.get(url)
 data = response.json()
 
-# Print output
 print("Name:", data["name"])
 print("ID:", data["id"])
